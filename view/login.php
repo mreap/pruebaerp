798aac7100fcacd9b5bf0c0a68057c13afbe1ec8
mreap <mrea@utn.edu.ec>
2023-07-05 14:02:05 -0500

    add login y menu

--- view/login.php
+++ view/login.php
@@ -5,8 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="https://cdn.simplecss.org/simple.css">
 </head>
 <body>
     <h3>Login al sistema</h3>
+    <form action="controller/controller.php">
+        <input type="submit" value="Login">
+    </form>
+    <a href="view/bancos/contador/index.php">Bancos - Contador</a>
 </body>
 </html>
