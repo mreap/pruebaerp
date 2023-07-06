5cc9ac3ac732465207eebc86877a7c9f2f28d49b
mreap <mrea@utn.edu.ec>
2023-07-05 21:31:21 -0500

    adicion de menu de ejemplo en seg admin

--- view/seguridades/admin/index.php
+++ view/seguridades/admin/index.php
@@ -7,6 +7,49 @@
     <title>Menu Seguridades Admin</title>
 </head>
 <body>
-    
+    <style>
+        body {
+  margin: 0;
+  padding: 0;
+}
+
+/* Estilos del menú */
+.menu {
+  width: 200px; /* Ancho del menú */
+  background-color: #f2f2f2;
+}
+
+.menu ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+
+.menu ul li a {
+  display: block;
+  padding: 10px;
+  text-decoration: none;
+  color: #333;
+}
+
+.menu ul li a:hover {
+  background-color: #ddd;
+}
+
+/* Estilos responsivos */
+@media screen and (max-width: 600px) {
+  .menu {
+    width: 100%; /* Menú ocupa todo el ancho en pantallas pequeñas */
+  }
+}
+    </style>
+    <div class="menu">
+    <ul>
+      <li><a href="#">Inicio</a></li>
+      <li><a href="#">Acerca de</a></li>
+      <li><a href="#">Servicios</a></li>
+      <li><a href="#">Contacto</a></li>
+    </ul>
+  </div>
 </body>
 </html>
